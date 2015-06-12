09c02c33e2bc6c241a9a745b029cd358f8c182b7
Thiago Lagden <lagden@gmail.com>
2015-06-12 16:22:15 -0300

    update deploy

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+THIS=$(basename $0)
+DIR="$( cd -P "$( dirname "$0" )" && pwd )"
+cd $DIR
+cd ..
+CURR_FOLDER=`pwd`
+
+# Root
+cd $CURR_FOLDER
+
 set -e
-rm -rf build || exit 0;
+rm -rf build || exit 0
 grunt build
 cd build
 git init
@@ -10,3 +19,4 @@ git config user.email "travis@lagden.in"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+exit 0
